f3c6d6a3ee8092af788806ad5ca18be001d020e1
lulin <lulin233@outlook.com>
2020-08-06 19:56:03 +0800

    把本地工程添加到git

---
+++ src/com/sy/demo01/Student.java
@@ -0,0 +1,14 @@
+package com.sy.demo01;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
